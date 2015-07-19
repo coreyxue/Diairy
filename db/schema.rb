1fc7a0c6adc347a12678a53dbb6e1f3a217b4b0d
Corey Xue <coreyxueatcal@gmail.com>
2015-07-18 23:52:04 -0700

    Add check_ins model, user can checkin for goals, no validation check yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718080817) do
+ActiveRecord::Schema.define(version: 20150719001224) do
+
+  create_table "check_ins", force: :cascade do |t|
+    t.datetime "datetime"
+    t.integer  "goal_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "check_ins", ["goal_id"], name: "index_check_ins_on_goal_id", using: :btree
 
   create_table "goals", force: :cascade do |t|
     t.date     "start_date"
@@ -60,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150718080817) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "check_ins", "goals"
   add_foreign_key "goals", "habits"
   add_foreign_key "goals", "users"
   add_foreign_key "user_informations", "users"

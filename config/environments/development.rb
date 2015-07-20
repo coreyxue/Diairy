88ae9cd31a83464e93d3e2ec1609908aebdbe8d3
Corey Xue <coreyxueatcal@gmail.com>
2015-07-19 23:32:22 -0700

    config mailer to send password reset instructions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => 'plain',
+    :user_name => "mkeeper96@gmail.com",
+    :password => "urnumber1"
+  }
 end
